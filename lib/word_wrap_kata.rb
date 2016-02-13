724c3bd0e75655cb7046a8c9b2a470ea6d066a58
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-02-13 15:29:25 -0600

    Corrected it
    Followed Uncle Bob’s steps

--- lib/word_wrap_kata.rb
+++ lib/word_wrap_kata.rb
@@ -3,11 +3,21 @@
 class WordWrapKata
 
   def wrap(string, max_length)
-
-    if string.length < max_length
+    if string.length <= max_length
       return string
-    else
-      return string[0, max_length] + "\n" + wrap(string[max_length..-1], max_length).rstrip
     end
+
+    if string[0...max_length].index(' ') != nil
+      space_position = (max_length - 1) - string[0...max_length].reverse.index(" ")
+      break_line(string, space_position, 1, max_length)
+    elsif string[max_length].eql?(" ")
+      break_line(string, max_length, 1, max_length)
+    else
+      break_line(string, max_length, 0, max_length)
+    end
+  end
+
+  def break_line(string, initial_pos, adjustment, max_length)
+    string[0...initial_pos] + "\n" + wrap(string[initial_pos + adjustment..-1], max_length)
   end
 end
--- spec/word_wrap_kata_spec.rb
+++ spec/word_wrap_kata_spec.rb
@@ -6,10 +6,34 @@ describe WordWrapKata do
     @word_wrap_kata = WordWrapKata.new
   end
 
+  it "empty string returns empty string" do
+    max_length = 1
+    string = ""
+    expect(@word_wrap_kata.wrap(string, max_length)).to eq string
+  end
+
   it "does not alter a string of less than max length" do
     max_length = 10
     string = "hello"
     expect(@word_wrap_kata.wrap(string, max_length)).to eq string
+  end
+
+  it "wraps two words with space" do
+    max_length = 6
+    string = "word word"
+    expect(@word_wrap_kata.wrap(string, max_length)).to eq "word\nword"
+  end
+
+  it "wraps three words after second space" do
+    max_length = 11
+    string = "word word word"
+    expect(@word_wrap_kata.wrap(string, max_length)).to eq "word word\nword"
+  end
+
+  it "splits single word many times" do
+    max_length = 3
+    string = "abcdefghij"
+    expect(@word_wrap_kata.wrap(string, max_length)).to eq "abc\ndef\nghi\nj"
   end
 
   it "breaks a string into two parts" do
@@ -24,34 +48,15 @@ describe WordWrapKata do
     expect(@word_wrap_kata.wrap(string, max_length)).to eq "he\nll\no"
   end
 
-  it "breaks a string into 5 parts with multiple words" do
-    max_length = 4
-    string = "hello, I am Sarah"
-    expect(@word_wrap_kata.wrap(string, max_length)).to eq "hell\no, I\n am \nSara\nh"
-  end
-
-  it "breaks a string into 2 parts" do
-    max_length = 9
-    string = "hello, I am Sarah"
-    expect(@word_wrap_kata.wrap(string, max_length)).to eq "hello, I \nam Sarah"
-  end
-
   it "breaks a string into 2 parts" do
     max_length = 2
     string = "mary"
     expect(@word_wrap_kata.wrap(string, max_length)).to eq "ma\nry"
   end
-
   it "breaks a string into 3 parts" do
     max_length = 1
     string = "dog"
     expect(@word_wrap_kata.wrap(string, max_length)).to eq "d\no\ng"
-  end
-
-  it "breaks a string into 3 parts" do
-    max_length = 2
-    string = "it is"
-    expect(@word_wrap_kata.wrap(string, max_length)).to eq "it\n i\ns"
   end
 
 end
